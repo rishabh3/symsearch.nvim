402d4d7c39b31649eef5ed4c90406c0509c685df
Rishabh Dutt <rishabh.dutt@citrix.com>
2021-11-20 16:12:07 +0530

    Initial commit

---
+++ lua/symsearch/init.lua
@@ -0,0 +1,11 @@
+local utils = require('symsearch.utils')
+
+local M = {}
+
+
+M.print = function()
+    print("Testing plugin changes")
+end
+
+
+return M
---
+++ lua/symsearch/utils/init.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+local function print_debug(a)
+    print(vim.inspect(a))
+end
+
+M.print_debug = print_debug
+
+return M
